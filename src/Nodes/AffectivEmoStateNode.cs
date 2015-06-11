6ec6a7f7c7826e49c22f13fbaf0039c303949c82
Renaud Gervais <renaud.gervais@gmail.com>
2015-06-11 12:04:04 +0100

    Adding new node for Affectiv processing

---
+++ src/Nodes/AffectivEmoStateNode.cs
@@ -0,0 +1,34 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Collections;
+using System.Collections.Generic;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using Emotiv;
+
+using VVVV.Core.Logging;
+#endregion usings
+
+
+namespace VVVV.EmotivEpoc
+{
+    #region PluginInfo
+    [PluginInfo(Name = "Affectiv",
+                Category = "EmoState",
+                Help = "Exposes the Affectiv properties of an EmoState, i.e. Excitement, Engagement, etc",
+                Tags = "Emotiv, Epoc, Affectiv, EmoState")]
+    #endregion PluginInfo
+    class AffectivEmoStateNode : IPluginEvaluate
+    {
+        //Processing loop
+        public void Evaluate(int SpreadMax)
+        {
+
+        }
+    }
+}
